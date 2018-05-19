9e189736750fdbf9841162de8cdefbfab79bbfe1
Jace <jacemcgough1@gmail.com>
2018-05-19 17:15:07 -0600

    Can select and disable buttons, changing colors not working

--- game.js
+++ game.js
@@ -1,8 +1,20 @@
 $(document).ready(  function() {
 
+  tilesPicked = 0
+
   // When a button is clicked 
   $('.buttons').on('click', function() {
-    pick = event.target.id
-    alert(pick)
+    pick = '#' + event.target.id
+    pickColor = "" + $(pick).prop('value')
+    $(pick).prop('disabled', true)
+    $(pick).prop('style', 'color:' + pickColor)
+    alert(pickColor)
   })
+
+  function pickTile(tile) {
+    tilesPicked++
+
+  }
+
+
 })
